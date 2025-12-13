bfa1fea0f9d8d858c63db69349777e46a27968e7
Radwa1055 <radwa.r2020@gmail.com>
2025-12-13 13:04:01 +0200

    Week 2 updates: pagination, filtering, search, README improvements

--- ecommerce_api/products/urls.py
+++ ecommerce_api/products/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import ProductListCreateView, ProductDetailView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CategoryViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='products')
+router.register(r'categories', CategoryViewSet, basename='categories')
 
 urlpatterns = [
-    path('', ProductListCreateView.as_view(), name='product_list'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('', include(router.urls)),
 ]
+
--- ecommerce_api/products/views.py
+++ ecommerce_api/products/views.py
@@ -1,16 +1,36 @@
-from rest_framework import generics
-from .models import Product
-from .serializers import ProductSerializer
+from rest_framework import viewsets, permissions, filters
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.exceptions import PermissionDenied
 
-# List + Create
-class ProductListCreateView(generics.ListCreateAPIView):
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = StandardResultsSetPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'category__name']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-# Retrieve + Update + Delete
-class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
+    def perform_update(self, serializer):
+        if serializer.instance.owner != self.request.user:
+            raise PermissionDenied("You cannot edit someone else's product")
+        serializer.save()
 
-
+    def perform_destroy(self, instance):
+        if instance.owner != self.request.user:
+            raise PermissionDenied("You cannot delete someone else's product")
+        instance.delete()
